6841691ac75dd82551911a2437e0ecf2ef9991c1
Nelson Earle <nelson.earle137@gmail.com>
2022-08-29 16:52:58 -0500

    impl Display formatting for Location

--- src/data/board.rs
+++ src/data/board.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use bevy::prelude::*;
 
 #[derive(Component)]
@@ -10,5 +12,13 @@ pub struct Location {
 impl Location {
     pub const fn new(file: u8, rank: u8, z: f32) -> Self {
         Self { file, rank, z }
+    }
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let file = (b'a' + self.file) as char;
+        let rank = self.rank + 1;
+        f.write_fmt(format_args!("{file}{rank}"))
     }
 }
