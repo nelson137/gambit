993958442318c007c7459707e0821a8ee280d237
Nelson Earle <nelson.earle137@gmail.com>
2023-01-19 23:41:04 -0600

    improve Load FEN Popup buttons

--- src/game/menu/fen_popup.rs
+++ src/game/menu/fen_popup.rs
@@ -41,12 +41,23 @@ pub(super) fn fen_menu(
 
 mod egui {
     use bevy_egui::egui::{
-        pos2, Align2, Context, Key, Rect, Response, Sense, TextEdit, TextStyle, Ui, Vec2, Window,
+        Align, Align2, Button, Color32, Context, Key, Layout, Response, RichText, TextBuffer,
+        TextEdit, TextStyle, Ui, Vec2, Window,
     };
 
     use crate::utils::UiSetTextStyleSize;
 
     use super::{FenPopupData, FenPopupInteraction};
+
+    /// `#7fa650`
+    const LOAD_BUTTON_COLOR: Color32 = Color32::from_rgb(0x7f, 0xa6, 0x50);
+
+    const LOAD_BUTTON_TEXT_COLOR: Color32 = Color32::WHITE;
+
+    const CANCEL_BUTTON_TEXT_COLOR: Color32 = Color32::WHITE;
+
+    /// `#ba2929`
+    const CANCEL_BUTTON_COLOR: Color32 = Color32::from_rgb(0xba, 0x29, 0x29);
 
     pub(super) fn fen_window(ctx: &Context, data: &mut FenPopupData) -> FenPopupInteraction {
         let mut interaction = FenPopupInteraction::None;
@@ -57,11 +68,8 @@ mod egui {
             .anchor(Align2::CENTER_CENTER, Vec2::ZERO)
             .show(ctx, |ui| {
                 ui.vertical_centered_justified(|mut ui| {
-                    if cancel_button(ui).clicked() {
-                        interaction = FenPopupInteraction::Cancel;
-                    }
-
                     ui.set_text_style_size(&TextStyle::Body, 24.0);
+                    ui.set_text_style_size(&TextStyle::Button, 24.0);
                     ui.set_text_style_size(&TextStyle::Heading, 32.0);
 
                     ui.heading("Load Game");
@@ -69,16 +77,32 @@ mod egui {
                     ui.separator();
 
                     const MARGIN: f32 = 12.0;
+
                     let contents_rect = ui.available_rect_before_wrap().shrink(MARGIN);
                     ui.allocate_ui_at_rect(contents_rect, |ui| {
                         ui.horizontal(|ui| {
                             ui.label("FEN:");
 
-                            let text_edit = TextEdit::singleline(&mut data.fen);
-                            let response = ui.add_sized(ui.available_size(), text_edit);
+                            let response = fen_text_edit(ui, &mut data.fen);
                             if response.lost_focus() && ui.input().key_pressed(Key::Enter) {
                                 interaction = FenPopupInteraction::Submit;
                             }
+                        });
+
+                        ui.add_space(MARGIN);
+
+                        ui.horizontal(|ui| {
+                            if cancel_button(ui).clicked() {
+                                interaction = FenPopupInteraction::Cancel;
+                            }
+
+                            let size = ui.available_rect_before_wrap().size();
+                            let layout = Layout::right_to_left(Align::Center);
+                            ui.allocate_ui_with_layout(size, layout, |ui| {
+                                if load_button(ui).clicked() {
+                                    interaction = FenPopupInteraction::Submit;
+                                }
+                            });
                         });
                     });
                     ui.add_space(MARGIN);
@@ -88,24 +112,17 @@ mod egui {
         interaction
     }
 
+    fn fen_text_edit(ui: &mut Ui, model: &mut dyn TextBuffer) -> Response {
+        ui.add_sized(ui.available_size(), TextEdit::singleline(model))
+    }
+
     fn cancel_button(ui: &mut Ui) -> Response {
-        let rect = ui.available_rect_before_wrap();
+        let text = RichText::new("Cancel").color(CANCEL_BUTTON_TEXT_COLOR);
+        ui.add(Button::new(text).fill(CANCEL_BUTTON_COLOR))
+    }
 
-        const PAD: f32 = 4.0;
-        let cancel_size = Vec2::splat(ui.spacing().icon_width - PAD);
-        let cancel_rect = Rect::from_min_size(
-            pos2(rect.right() - cancel_size.x - PAD, rect.top() + PAD),
-            cancel_size,
-        );
-
-        let cancel_id = ui.auto_id_with("cancel_button");
-        let response = ui.interact(cancel_rect, cancel_id, Sense::click());
-
-        let painter = ui.painter();
-        let stroke = ui.style().interact(&response).fg_stroke;
-        painter.line_segment([cancel_rect.left_top(), cancel_rect.right_bottom()], stroke);
-        painter.line_segment([cancel_rect.left_bottom(), cancel_rect.right_top()], stroke);
-
-        response
+    fn load_button(ui: &mut Ui) -> Response {
+        let text = RichText::new("Load").color(LOAD_BUTTON_TEXT_COLOR);
+        ui.add(Button::new(text).fill(LOAD_BUTTON_COLOR))
     }
 }
