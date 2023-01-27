fe68913325215a696b068e59b78ccf35fadb0e6a
Nelson Earle <nelson.earle137@gmail.com>
2023-01-27 16:19:43 -0600

    put move hints above highlight tiles

--- src/game/consts.rs
+++ src/game/consts.rs
@@ -19,9 +19,9 @@ pub const Z_PIECE_SELECTED: i32 = 11;
 
 pub const Z_PIECE: i32 = 10;
 
-pub const Z_HIGHLIGHT_TILE: i32 = 4;
+pub const Z_MOVE_HINT: i32 = 4;
 
-pub const Z_MOVE_HINT: i32 = 3;
+pub const Z_HIGHLIGHT_TILE: i32 = 3;
 
 pub const Z_NOTATION_TEXT: i32 = 2;
 
