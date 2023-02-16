875d04270fe69f15b712bc6b7f82ea232d1c940d
Nelson Earle <nelson.earle137@gmail.com>
2023-02-16 00:13:21 -0600

    add FEN builder
    Add responsive controls to the FEN input popup that allow manipulating
    the FEN. These controls include a toggle for whose turn it is and castle
    rights.
    
    TODO:
    
    - En passant target
    - Halfmove clock & fullmove count

--- src/game/menu/fen_popup.rs
+++ src/game/menu/fen_popup.rs
@@ -1,20 +1,158 @@
-use std::str::FromStr;
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+    ops::{Deref, DerefMut},
+    str::FromStr,
+};
 
 use bevy::prelude::*;
 use bevy_egui::EguiContext;
+use chess::{Board, BoardBuilder, CastleRights};
 
 use crate::{game::load::LoadGame, utils::StateExts};
 
 use super::MenuState;
 
-#[derive(Resource, Default)]
+const DEFAULT_BOARD_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+fn compute_hash<T: Hash>(value: &T) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    value.hash(&mut hasher);
+    hasher.finish()
+}
+
+#[derive(Resource)]
 pub(super) struct FenPopupData {
     fen: String,
+    fen_hash: u64,
+    controls: FenPopupDataControls,
+    controls_hash: u64,
+    invalid_fen: bool,
+}
+
+impl Default for FenPopupData {
+    fn default() -> Self {
+        let controls = FenPopupDataControls::default();
+        let fen = String::new();
+        Self {
+            controls_hash: compute_hash(&controls),
+            controls,
+            fen_hash: compute_hash(&fen),
+            fen,
+            invalid_fen: false,
+        }
+    }
+}
+
+impl FenPopupData {
+    fn sync(&mut self) {
+        let mut fen_hash = compute_hash(&self.fen);
+        let mut controls_hash = compute_hash(&self.controls);
+
+        if fen_hash != self.fen_hash {
+            // Update the controls
+
+            let board = Board::from_str(&self.fen);
+            self.invalid_fen = board.is_err();
+
+            if let Ok(board) = board {
+                self.black_to_move = board.side_to_move() == chess::Color::Black;
+
+                let white_castle_rights = board.castle_rights(chess::Color::White);
+                self.castle_rights_white_kingside = white_castle_rights.has_kingside();
+                self.castle_rights_white_queenside = white_castle_rights.has_queenside();
+
+                let black_castle_rights = board.castle_rights(chess::Color::Black);
+                self.castle_rights_black_kingside = black_castle_rights.has_kingside();
+                self.castle_rights_black_queenside = black_castle_rights.has_queenside();
+
+                controls_hash = compute_hash(&self.controls);
+            }
+        } else if controls_hash != self.controls_hash {
+            // Update the FEN
+
+            if self.fen.is_empty() {
+                self.fen.replace_range(.., DEFAULT_BOARD_FEN);
+            }
+
+            let board = BoardBuilder::from_str(&self.fen);
+            self.invalid_fen = board.is_err();
+            if let Ok(mut board) = board {
+                let fen_black_to_move = board.get_side_to_move() == chess::Color::Black;
+                if fen_black_to_move != self.black_to_move {
+                    board.side_to_move(!board.get_side_to_move());
+                }
+
+                board.castle_rights(chess::Color::White, self.white_castle_rights());
+                board.castle_rights(chess::Color::Black, self.black_castle_rights());
+
+                self.fen = board.to_string();
+                fen_hash = compute_hash(&self.fen);
+            }
+        }
+
+        self.fen_hash = fen_hash;
+        self.controls_hash = controls_hash;
+    }
+}
+
+impl Deref for FenPopupData {
+    type Target = FenPopupDataControls;
+
+    fn deref(&self) -> &Self::Target {
+        &self.controls
+    }
+}
+
+impl DerefMut for FenPopupData {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.controls
+    }
 }
 
 impl FenPopupData {
     pub fn reset(&mut self) {
         self.fen.clear();
+    }
+}
+
+#[derive(Hash)]
+pub(super) struct FenPopupDataControls {
+    black_to_move: bool,
+    castle_rights_white_kingside: bool,
+    castle_rights_white_queenside: bool,
+    castle_rights_black_kingside: bool,
+    castle_rights_black_queenside: bool,
+}
+
+impl Default for FenPopupDataControls {
+    fn default() -> Self {
+        Self {
+            black_to_move: false,
+            castle_rights_white_kingside: true,
+            castle_rights_white_queenside: true,
+            castle_rights_black_kingside: true,
+            castle_rights_black_queenside: true,
+        }
+    }
+}
+
+impl FenPopupDataControls {
+    fn white_castle_rights(&self) -> CastleRights {
+        castle_rights(self.castle_rights_white_kingside, self.castle_rights_white_queenside)
+    }
+
+    fn black_castle_rights(&self) -> CastleRights {
+        castle_rights(self.castle_rights_black_kingside, self.castle_rights_black_queenside)
+    }
+}
+
+fn castle_rights(kingside: bool, queenside: bool) -> CastleRights {
+    match (kingside, queenside) {
+        (true, true) => CastleRights::Both,
+        (true, false) => CastleRights::KingSide,
+        (false, true) => CastleRights::QueenSide,
+        (false, false) => CastleRights::NoRights,
     }
 }
 
@@ -44,13 +182,15 @@ pub(super) fn fen_menu(
 
 mod egui {
     use bevy_egui::egui::{
-        vec2, Align, Align2, Button, Color32, Context, Key, Layout, Response, RichText, TextBuffer,
-        TextEdit, TextStyle, Ui, Vec2, Window,
+        lerp, pos2, vec2, Align, Align2, Button, Color32, Context, Frame, Key, Layout, Response,
+        RichText, Sense, Stroke, TextBuffer, TextEdit, TextStyle, Ui, Vec2, WidgetInfo, WidgetType,
+        Window,
     };
+    use egui_extras::{Size, StripBuilder};
 
     use crate::utils::UiSetTextStyleSize;
 
-    use super::{FenPopupData, FenPopupInteraction};
+    use super::{FenPopupData, FenPopupInteraction, DEFAULT_BOARD_FEN};
 
     /// `#7fa650`
     const LOAD_BUTTON_COLOR: Color32 = Color32::from_rgb(0x7f, 0xa6, 0x50);
@@ -76,20 +216,19 @@ mod egui {
                 }
 
                 ui.vertical_centered_justified(|mut ui| {
-                    ui.set_text_style_size(&TextStyle::Body, 24.0);
+                    ui.set_text_style_size(&TextStyle::Body, 20.0);
                     ui.set_text_style_size(&TextStyle::Button, 24.0);
                     ui.set_text_style_size(&TextStyle::Heading, 32.0);
 
                     ui.heading("Load Game");
-
                     ui.separator();
 
-                    const MARGIN: f32 = 12.0;
+                    ui.set_min_size(vec2(768.0, 0.0));
+                    Frame::none().outer_margin(12.0).show(ui, |ui| {
+                        const SPACING: f32 = 24.0;
 
-                    let contents_rect = ui.available_rect_before_wrap().shrink(MARGIN);
-                    ui.allocate_ui_at_rect(contents_rect, |ui| {
                         ui.horizontal(|ui| {
-                            ui.label("FEN:");
+                            ui.label(RichText::new("FEN:").underline());
 
                             let response = fen_text_edit(ui, &mut data.fen);
                             if response.lost_focus() && ui.input().key_pressed(Key::Enter) {
@@ -97,7 +236,22 @@ mod egui {
                             }
                         });
 
-                        ui.add_space(MARGIN);
+                        ui.add_space(SPACING);
+
+                        ui.horizontal(|ui| {
+                            ui.label(RichText::new("Side to move:").underline());
+                            players_turn_label_white(ui, data.black_to_move);
+                            players_turn_toggle(ui, &mut data.black_to_move);
+                            players_turn_label_black(ui, data.black_to_move);
+                        });
+
+                        ui.add_space(SPACING);
+
+                        ui.horizontal(|ui| {
+                            castle_rights_check_boxes(ui, data);
+                        });
+
+                        ui.add_space(SPACING);
 
                         ui.horizontal(|ui| {
                             if cancel_button(ui).clicked() {
@@ -113,15 +267,81 @@ mod egui {
                             });
                         });
                     });
-                    ui.add_space(MARGIN);
                 });
             });
+
+        data.sync();
+
+        if let FenPopupInteraction::Submit = interaction {
+            data.fen.replace_range(.., DEFAULT_BOARD_FEN);
+        }
 
         interaction
     }
 
     fn fen_text_edit(ui: &mut Ui, model: &mut dyn TextBuffer) -> Response {
-        ui.add_sized(vec2(691.0, ui.available_size().y), TextEdit::singleline(model))
+        ui.add(
+            TextEdit::singleline(model).hint_text(DEFAULT_BOARD_FEN).desired_width(f32::INFINITY),
+        )
+    }
+
+    fn players_turn_label_white(ui: &mut Ui, black_to_move: bool) {
+        let text = RichText::new("White");
+        ui.label(if black_to_move { text } else { text.strong() });
+    }
+
+    fn players_turn_label_black(ui: &mut Ui, black_to_move: bool) {
+        let text = RichText::new("Black");
+        ui.label(if black_to_move { text.strong() } else { text });
+    }
+
+    // Taken from: https://github.com/emilk/egui/blob/041f2e64bac778c9095fbf4316dc1f7c7bceb670/crates/egui_demo_lib/src/demo/toggle_switch.rs
+    fn players_turn_toggle(ui: &mut Ui, value: &mut bool) -> Response {
+        let desired_size = ui.spacing().interact_size.y * vec2(2.5, 1.0);
+        let (rect, mut response) = ui.allocate_exact_size(desired_size, Sense::click());
+        if response.clicked() {
+            *value = !*value;
+            response.mark_changed();
+        }
+        response.widget_info(|| WidgetInfo::selected(WidgetType::Checkbox, *value, ""));
+
+        if ui.is_rect_visible(rect) {
+            let visuals = ui.style().interact_selectable(&response, *value);
+
+            let fill_color = if *value { Color32::DARK_GRAY } else { Color32::LIGHT_GRAY };
+            let stroke_color = if *value { Color32::GRAY } else { Color32::BLACK };
+            let stroke = Stroke::new(visuals.fg_stroke.width, stroke_color);
+
+            let rect = rect.expand(visuals.expansion);
+            let radius = 0.5 * rect.height();
+            ui.painter().rect(rect, radius, fill_color, stroke);
+            let how_on = ui.ctx().animate_bool(response.id, *value);
+            let circle_x = lerp((rect.left() + radius)..=(rect.right() - radius), how_on);
+            let center = pos2(circle_x, rect.center().y);
+            ui.painter().circle(center, 0.75 * radius, fill_color, stroke);
+        }
+
+        response
+    }
+
+    fn castle_rights_check_boxes(ui: &mut Ui, data: &mut FenPopupData) {
+        let strip_builder = StripBuilder::new(ui).sizes(Size::initial(256.0), 2);
+        strip_builder.horizontal(|mut strip| {
+            strip.cell(|ui| {
+                ui.vertical(|ui| {
+                    ui.label(RichText::new("White Castle Rights").underline());
+                    ui.checkbox(&mut data.castle_rights_white_kingside, "Kingside (O-O)");
+                    ui.checkbox(&mut data.castle_rights_white_queenside, "Queenside (O-O-O)");
+                });
+            });
+            strip.cell(|ui| {
+                ui.vertical(|ui| {
+                    ui.label(RichText::new("Black Castle Rights").underline());
+                    ui.checkbox(&mut data.castle_rights_black_kingside, "Kingside (O-O)");
+                    ui.checkbox(&mut data.castle_rights_black_queenside, "Queenside (O-O-O)");
+                });
+            });
+        });
     }
 
     fn cancel_button(ui: &mut Ui) -> Response {
