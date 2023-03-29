347dc015b35b7c6701ba6eaf24e91ca5ad527439
Nelson Earle <nelson.earle137@gmail.com>
2023-03-29 15:36:09 -0700

    refactor hard-coded piece asset paths with macro

--- src/assets/pieces.rs
+++ src/assets/pieces.rs
@@ -1,5 +1,11 @@
 use crate::game::board::{PieceColor, PieceType};
 
+macro_rules! asset_path {
+    ($color:literal, $type:literal) => {
+        concat!("images/pieces/", $color, "-", $type, ".png")
+    };
+}
+
 pub trait PieceColorAndTypeAssetPath {
     fn asset_path(self) -> &'static str;
 }
@@ -7,18 +13,18 @@ pub trait PieceColorAndTypeAssetPath {
 impl PieceColorAndTypeAssetPath for (PieceColor, PieceType) {
     fn asset_path(self) -> &'static str {
         match (self.0, self.1) {
-            (PieceColor::BLACK, PieceType::BISHOP) => "images/pieces/black-bishop.png",
-            (PieceColor::BLACK, PieceType::KING) => "images/pieces/black-king.png",
-            (PieceColor::BLACK, PieceType::KNIGHT) => "images/pieces/black-knight.png",
-            (PieceColor::BLACK, PieceType::PAWN) => "images/pieces/black-pawn.png",
-            (PieceColor::BLACK, PieceType::QUEEN) => "images/pieces/black-queen.png",
-            (PieceColor::BLACK, PieceType::ROOK) => "images/pieces/black-rook.png",
-            (PieceColor::WHITE, PieceType::BISHOP) => "images/pieces/white-bishop.png",
-            (PieceColor::WHITE, PieceType::KING) => "images/pieces/white-king.png",
-            (PieceColor::WHITE, PieceType::KNIGHT) => "images/pieces/white-knight.png",
-            (PieceColor::WHITE, PieceType::PAWN) => "images/pieces/white-pawn.png",
-            (PieceColor::WHITE, PieceType::QUEEN) => "images/pieces/white-queen.png",
-            (PieceColor::WHITE, PieceType::ROOK) => "images/pieces/white-rook.png",
+            (PieceColor::BLACK, PieceType::BISHOP) => asset_path!("black", "bishop"),
+            (PieceColor::BLACK, PieceType::KING) => asset_path!("black", "king"),
+            (PieceColor::BLACK, PieceType::KNIGHT) => asset_path!("black", "knight"),
+            (PieceColor::BLACK, PieceType::PAWN) => asset_path!("black", "pawn"),
+            (PieceColor::BLACK, PieceType::QUEEN) => asset_path!("black", "queen"),
+            (PieceColor::BLACK, PieceType::ROOK) => asset_path!("black", "rook"),
+            (PieceColor::WHITE, PieceType::BISHOP) => asset_path!("white", "bishop"),
+            (PieceColor::WHITE, PieceType::KING) => asset_path!("white", "king"),
+            (PieceColor::WHITE, PieceType::KNIGHT) => asset_path!("white", "knight"),
+            (PieceColor::WHITE, PieceType::PAWN) => asset_path!("white", "pawn"),
+            (PieceColor::WHITE, PieceType::QUEEN) => asset_path!("white", "queen"),
+            (PieceColor::WHITE, PieceType::ROOK) => asset_path!("white", "rook"),
         }
     }
 }
